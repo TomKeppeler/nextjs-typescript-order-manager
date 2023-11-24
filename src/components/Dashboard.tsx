c18ba0536a1ad06a73c86cc4236e11af30cbd9c4
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2023-11-24 12:26:53 +0100

    added dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,8 @@
+import {TableBody} from "@mui/material";
+import SelectableTable from "@../components/mui/SelectableTable";
+import React from "react";
+
+export default function Dashboard(props: any){
+    {const tableName = props.tableName;}
+    return <SelectableTable></SelectableTable>;
+}
